antonblanchard/qtrace-tools

commit e7393fc4ded959cc3a16d8bf25f8fce35b3df678

ptracer qtwriter port
Port ptracer over to qtwriter lib. This also moves ptracer/branch.h
out to qtlib/branch.h

ptracer/main.c
@@ -11,11 +11,16 @@
 #include <sys/resource.h>
 #include <linux/ptrace.h>
 
+#include <qtwriter.h>
+
+#include "../qtlib/branch.h"
+
+static struct qtwriter_state qtwr;
+
 #include "pids.h"
 #include "ptrace.h"
 #include "perf_events.h"
 #include "single_step.h"
-#include "qtrace.h"
 #include "ascii.h"
 #include "ppc_storage.h"
 
@@ -64,6 +69,19 @@ static void setrlimit_open_files(void)
 	}
 }
 
+static char *ascii_logfile = NULL;
+static char *qtrace_logfile = NULL;
+static unsigned long nr_insns_skip = 0;
+static unsigned long nr_insns_left = -1UL;
+
+struct register_state {
+	struct pt_regs regs;
+	__int128 vmx_regs[32+2];
+	__int128 vsx_regs[32];
+	unsigned int fpscr;
+};
+static struct register_state prev_state;
+
 static void do_checkpoint(pid_t pid, char *dir)
 {
 	char pidstr[8];
@@ -80,21 +98,10 @@ static void do_checkpoint(pid_t pid, char *dir)
 		perror("do_checkpoint: execl");
 	}
 
+	if (qtrace_logfile)
+		qtwriter_close(&qtwr);
 	exit(0);
 }
-
-static char *ascii_logfile = NULL;
-static char *qtrace_logfile = NULL;
-static unsigned long nr_insns_skip = 0;
-static unsigned long nr_insns_left = -1UL;
-
-struct register_state {
-	struct pt_regs regs;
-	__int128 vmx_regs[32+2];
-	__int128 vsx_regs[32];
-	unsigned int fpscr;
-};
-static struct register_state prev_state;
 
 static void get_gp_regs(unsigned long pid, struct register_state *state)
 {
@@ -239,6 +246,7 @@ static void print_insn(pid_t pid, uint32_t *pc)
 	}
 
 	if (qtrace_logfile) {
+		struct qtrace_record qtr;
 		int ret;
 		struct pt_regs regs;
 		unsigned long addr = 0, size = 0;
@@ -249,10 +257,23 @@ static void print_insn(pid_t pid, uint32_t *pc)
 			exit(1);
 		}
 
-		if (is_storage_insn(insn, &regs.gpr[0], &addr, &size))
-			qtrace_add_storage_record(insn, pc, addr, size);
-		else
-			qtrace_add_record(insn, pc);
+		memset(&qtr, 0, sizeof(qtr));
+		qtr.insn = insn;
+		qtr.insn_addr = (unsigned long)pc;
+
+		if (is_storage_insn(insn, &regs.gpr[0], &addr, &size)) {
+			qtr.data_addr = addr;
+			qtr.data_addr_valid = true;
+		}
+
+		qtr.branch = is_branch(insn);
+		qtr.conditional_branch = is_conditional_branch(insn);
+		qtr.conditional_branch = true;
+
+		if (qtwriter_write_record(&qtwr, &qtr) == false) {
+			fprintf(stderr, "qtwriter_write_record failed\n");
+			exit(1);
+		}
 	}
 }
 
@@ -335,7 +356,7 @@ int main(int argc, char *argv[])
 		ascii_fout = stdout;
 	} else {
 		if (qtrace_logfile)
-			qtrace_open(qtrace_logfile);
+			qtwriter_open(&qtwr, qtrace_logfile, 0);
 
 		if (ascii_logfile)
 			ascii_open(ascii_logfile);
@@ -417,7 +438,8 @@ int main(int argc, char *argv[])
 			remove_pid(pid);
 
 			if (pid == tracing_pid) {
-				qtrace_close();
+				if (qtrace_logfile)
+					qtwriter_close(&qtwr);
 				exit(0);
 			}
 
@@ -499,6 +521,8 @@ int main(int argc, char *argv[])
 
 			if (nr_insns_left == 0) {
 				detach_all_threads();
+				if (qtrace_logfile)
+					qtwriter_close(&qtwr);
 				exit(0);
 			}
 

ptracer/qtrace.c
@@ -1,217 +1,0 @@
-/*
- * Copyright (C) 2017 Anton Blanchard <anton@au.ibm.com>, IBM
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or (at your option) any later version.
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#include "branch.h"
-
-#include <qtrace.h>
-
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#define be16_to_cpup(A)	__builtin_bswap16(*(uint16_t *)(A))
-#define be32_to_cpup(A)	__builtin_bswap32(*(uint32_t *)(A))
-#define be64_to_cpup(A)	__builtin_bswap64(*(uint64_t *)(A))
-#define cpu_to_be16(A) __builtin_bswap16(A)
-#define cpu_to_be32(A) __builtin_bswap32(A)
-#define cpu_to_be64(A) __builtin_bswap64(A)
-#else
-#define be16_to_cpup(A)	(*(uint16_t *)A)
-#define be32_to_cpup(A)	(*(uint32_t *)A)
-#define be64_to_cpup(A)	(*(uint64_t *)A)
-#define cpu_to_be16(A) (A)
-#define cpu_to_be32(A) (A)
-#define cpu_to_be64(A) (A)
-#endif
-
-static int fd;
-
-void qtrace_open(char *filename)
-{
-	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0644);
-	if (fd == -1) {
-		perror("open");
-		exit(1);
-	}
-}
-
-static bool handle_branch(uint32_t insn, unsigned long insn_addr);
-
-void qtrace_close(void)
-{
-	/* If the previous instruction was a branch, write it out. */
-	handle_branch(0, 0);
-	close(fd);
-}
-
-/*
- * The header contains the address of the first instruction, so we can't
- * write it until we get the first trace entry.
- */
-static bool header_written = false;
-static void write_header(unsigned long addr)
-{
-	uint16_t flags;
-	uint32_t t32;
-	uint64_t t64;
-	ssize_t ret;
-
-	/* Header is identified by a zero instruction */
-	t32 = 0;
-	ret = write(fd, &t32, sizeof(t32));
-	assert(ret == sizeof(t32));
-
-	flags = cpu_to_be16(QTRACE_EXTENDED_FLAGS_PRESENT);
-	ret = write(fd, &flags, sizeof(flags));
-	assert(ret == sizeof(flags));
-
-	flags = cpu_to_be16(QTRACE_FILE_HEADER_PRESENT);
-	ret = write(fd, &flags, sizeof(flags));
-	assert(ret == sizeof(flags));
-
-	flags = cpu_to_be16(QTRACE_HDR_VERSION_NUMBER_PRESENT | QTRACE_HDR_IAR_PRESENT);
-	ret = write(fd, &flags, sizeof(flags));
-	assert(ret == sizeof(flags));
-
-	t32 = cpu_to_be32(0); /* XXX FIXME */
-	ret = write(fd, &t32, sizeof(t32));
-	assert(ret == sizeof(t32));
-
-	t64 = cpu_to_be64(addr);
-	ret = write(fd, &t64, sizeof(t64));
-	assert(ret == sizeof(t64));
-}
-
-static bool handle_branch(uint32_t insn, unsigned long insn_addr)
-{
-	static uint32_t prev_branch_insn = 0;
-	static unsigned long prev_branch_addr = 0;
-	ssize_t ret;
-
-	if (prev_branch_insn) {
-		uint8_t t8;
-		uint16_t flags = QTRACE_NODE_PRESENT | QTRACE_TERMINATION_PRESENT;
-		uint32_t t32;
-		uint64_t t64;
-		bool iar_change = false;
-
-		/*
-		 * A zero instruction is used to write out any previous branch
-		 * before closing the qtrace file.
-		 */
-		if (insn && ((prev_branch_addr + 4) != insn_addr))
-			iar_change = true;
-
-		if (iar_change)
-			flags |= (QTRACE_IAR_CHANGE_PRESENT | QTRACE_IAR_PRESENT);
-
-		t32 = cpu_to_be32(prev_branch_insn);
-		ret = write(fd, &t32, sizeof(t32));
-		assert(ret == sizeof(t32));
-
-		flags = cpu_to_be16(flags);
-		ret = write(fd, &flags, sizeof(flags));
-		assert(ret == sizeof(flags));
-
-		/* node */
-		t8 = 0;
-		ret = write(fd, &t8, sizeof(t8));
-		assert(ret == sizeof(t8));
-
-		/* termination node */
-		t8 = 0;
-		ret = write(fd, &t8, sizeof(t8));
-		assert(ret == sizeof(t8));
-
-		/* termination code */
-		t8 = 0;
-		if (is_conditional_branch(prev_branch_insn))
-			t8 = QTRACE_EXCEEDED_MAX_INST_DEPTH;
-
-		if (is_unconditional_branch(prev_branch_insn))
-			t8 = QTRACE_UNCONDITIONAL_BRANCH;
-
-		ret = write(fd, &t8, sizeof(t8));
-		assert(ret == sizeof(t8));
-
-		if (iar_change) {
-			t64 = cpu_to_be64(insn_addr);
-			ret = write(fd, &t64, sizeof(t64));
-			assert(ret == sizeof(t64));
-		}
-	}
-
-	if (is_branch(insn)) {
-		prev_branch_insn = insn;
-		prev_branch_addr = insn_addr;
-		return true;
-	}
-
-	prev_branch_insn = 0;
-	return false;
-}
-
-void qtrace_add_record(uint32_t insn, unsigned long insn_addr)
-{
-	uint16_t flags;
-	uint32_t t32;
-	ssize_t ret;
-
-	if (!header_written) {
-		header_written = true;
-		write_header(insn_addr);
-	}
-
-	if (handle_branch(insn, insn_addr))
-		return;
-
-	t32 = cpu_to_be32(insn);
-	ret = write(fd, &t32, sizeof(t32));
-	assert(ret == sizeof(t32));
-
-	flags = 0;
-	ret = write(fd, &flags, sizeof(flags));
-	assert(ret == sizeof(flags));
-}
-
-void qtrace_add_storage_record(uint32_t insn, unsigned long insn_addr,
-			       unsigned long storage_addr,
-			       unsigned long storage_size)
-{
-	uint16_t flags;
-	uint32_t t32;
-	uint64_t t64;
-	ssize_t ret;
-
-	if (!header_written) {
-		header_written = true;
-		write_header(insn_addr);
-	}
-
-	handle_branch(insn, insn_addr);
-
-	t32 = cpu_to_be32(insn);
-	ret = write(fd, &t32, sizeof(t32));
-	assert(ret == sizeof(t32));
-
-	flags = cpu_to_be16(QTRACE_DATA_ADDRESS_PRESENT);
-	ret = write(fd, &flags, sizeof(flags));
-	assert(ret == sizeof(flags));
-
-	t64 = cpu_to_be64(storage_addr);
-	ret = write(fd, &t64, sizeof(t64));
-	assert(ret == sizeof(t64));
-}

ptracer/qtrace.h
@@ -1,13 +1,0 @@
-#ifndef __QTRACE__OLD_H__
-#define __QTRACE__OLD_H__
-
-#include <stdint.h>
-
-void qtrace_open(char *filename);
-void qtrace_close(void);
-void qtrace_add_record(uint32_t insn, uint32_t *insn_addr);
-void qtrace_add_storage_record(uint32_t insn, uint32_t *insn_addr,
-			       unsigned long storage_addr,
-			       unsigned long storage_size);
-
-#endif

qtbuild/qtbuild.c
@@ -35,7 +35,7 @@
 #include <sys/stat.h>
 
 #include <qtwriter.h>
-#include "../ptracer/branch.h"
+#include "../qtlib/branch.h"
 #include "../ptracer/ppc_storage.h"
 #include "../ptracer/ppc_storage.c"
 

commit 79dd7db37eecaf95738d684401b81ecae607bece

ptracer: Fix conditional branch handling
commit e7393fc4ded9 ("ptracer qtwriter port") introduced a bug
where we always marked a branch as conditional.

Signed-off-by: Anton Blanchard <anton@ozlabs.org>

ptracer/main.c
@@ -285,7 +285,6 @@ static void print_insn(pid_t pid, uint32_t *pc)
 
 		qtr.branch = is_branch(insn);
 		qtr.conditional_branch = is_conditional_branch(insn);
-		qtr.conditional_branch = true;
 
 		if (qtwriter_write_record(&qtwr, &qtr) == false) {
 			fprintf(stderr, "qtwriter_write_record failed\n");